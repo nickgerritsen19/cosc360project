9f5c713965386e765a39d271e94a9052d31cf5a9
Nick <nicholas.gerritsen@hotmail.com>
2020-04-07 20:18:09 -0600

    Header changed to php to display logged in user

---
+++ header.php
@@ -0,0 +1,25 @@
+<a href="home.html"><img id="logo" src="images/Logo.jpg" alt="Logo"></a>
+<h1>Lettuce Get Some Deals</h1>
+<form method="GET" action="">
+<input type="text" size="100" name="search" placeholder="Search Here"/>
+<a href="#search"><img id="search" src="images/search_icon.png" alt="search_icon" height="15"></a>
+</form>
+<nav>
+    <p>
+        <a href="#all-products">All Products</a>
+        <a href="#categories">Categories</a>
+        <a href="#best-deals">Best Deals</a>
+        <a href="#about-us">About Us</a>
+        <a href="#admin">Admin</a>
+        <?php
+            session_start();
+            if($_SESSION['loggedIn'] == true) {
+                printf("<a href=\"userprofile.html\">%s</a>",$_SESSION['username']);
+                printf("<a href=\"signout.php\">Sign Out</a>");
+            }else {
+                echo '<a href="login.html">Log In</a>';
+                echo '<a href="createuser.html">Sign Up</a>';
+            }
+        ?>
+    </p>
+</nav> 
