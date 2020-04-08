0fa8e6c267b64d16560aa812bed3fb680e24c76f
maryw1 <whitten.mary175@gmail.com>
2020-04-08 15:51:32 -0700

    Upfdated submitreviews.html and .php

--- submitreview.php
+++ submitreview.php
@@ -1,24 +1,27 @@
 <?php
-require_once('db_connect.php');
+    session_start();
+    if($_SESSION['loggedIn'] == true) {
+        require_once('db_connect.php');
+        $sql = "SELECT username FROM users WHERE username = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("s", $_SESSION["username"]);
+        $stmt->execute();
+        $username = $stmt->get_result();
 
-    //Need to figgure out how to get username when logged in//
-    //$username = 
-    $review = $_POST["review"];    
-    $rating = $_POST["rating"];
-    $productid = $_POST["productid"];
-
-    $sql = "INSERT INTO reviews (username, reviewcontent, rating, productid) VALUES (?,?,?,?)";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("ssss", $username, $review, $rating, $productid);
-    $stmt->execute();
-    $stmt->close();
-    $conn->close();
+        $review = $_POST["review"];    
+        $rating = $_POST["rating"];
+        $productid = $_POST["productid"];
     
-    $newURL = "product.html";
-
-    header('Location: '.$newURL);
+        $sql = "INSERT INTO reviews (username, reviewcontent, rating, productid) VALUES (?,?,?,?)";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("ssss", $username, $review, $rating, $productid);
+        $stmt->execute();
+        $stmt->close();
+        $conn->close();
+        
+        $newURL = "product.html";
     
-    
+        header('Location: '.$newURL);
 
-
+    }
 ?>
