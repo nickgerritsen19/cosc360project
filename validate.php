a493e7ed04f9c2e32ae69bcdcc5c85d306c39010
Nick <nicholas.gerritsen@hotmail.com>
2020-04-07 20:18:32 -0600

    Log In feature working

--- validate.php
+++ validate.php
@@ -1,29 +1,43 @@
 <?php
-    define('DBHOST', 'localhost');
-    define('DBNAME', 'testdatabase');
-    define('DBUSER', 'root');
-    define('DBPASS', 'root');
+    require_once('db_connect.php');
 
-    // Create connection
-    $conn = new mysqli(DBHOST, DBUSER, DBPASS, DBNAME);
+    session_start();
 
-    $username = $_POST["username"];
-    $password = $_POST["password"];
+    $_SESSION['loggedIn'] = false;
 
-    $sql = "SELECT username, password FROM users WHERE username = ?";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("s", $username);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    $row = $result->fetch_assoc();
-    $DBpassword = $row['password'];
-    if($DBpassword == $password) {
-        header('Location: home.html');
-    }
-    if($DBpassword != $password)
+    /** Get customer id **/
+    $username = null;
+    if(isset($_POST['username']))
+        $username = $_POST['username'];
+    if($username == null)
+        die("No username entered.");
+
+    //get customer password
+    $password = "";
+    if(isset($_POST['password']))
+        $password = $_POST['password'];
+    if($_POST['password'] == "")
+        die("No password entered.");
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST"){
+        $stmt = $conn->prepare("SELECT username, password FROM users WHERE username=? AND password=?");
+        $stmt->bind_param( "ss", $username,$password);
+            $stmt->execute();
+            $stmt->store_result();
+            $rows = $stmt->num_rows;
+            if($rows == 1){
+                //user is found, set session variable and redirect
+                $_SESSION['username'] = $username;
+                $_SESSION['loggedIn'] = true;
+                header('Location: home.html');
+            }else{
+                $_SESSION['errMsg'] = "<p style='color:red' align='center'> Invalid username/password! </p>";
+                //username and password combo do not exist
+                header('Location: login.php');
+            }
+            $conn->close();
+    }else{
+        //user is trying to access page indirectly, redirect to login page
         header('Location: login.php');
-
-
-    $stmt->close();
-    $conn->close();
+    }
 ?>
