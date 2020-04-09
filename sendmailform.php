725e4b279fa0008a479ab43e0d1620ee747f9dd3
maryw1 <whitten.mary175@gmail.com>
2020-04-09 13:46:39 -0700

    sendmailform update

--- sendmailform.php
+++ sendmailform.php
@@ -1,14 +1,38 @@
 <html>
-<table width=”400″ border=”0″ align=”center” cellpadding=”3″ cellspacing=”1″>
-<tr>
-<td><strong>Send mail Form</strong></td>
-</tr>
-</table>
-<table width=”400″ border=”0″ align=”center” cellpadding=”0″ cellspacing=”1″>
-<tr>
-<td><form name=”form1″ method=”post” action=”mailto.php”>
-<table width=”100%” border=”0″ cellspacing=”1″ cellpadding=”3″>
-<tr>
+    <form method="POST" id="mainForm" action="mailto.php">
+        <fieldset>
+            <legend>Send Mail Form</legend>
+            <p>
+                <label>Subject:</label></br>
+                <input type="text" name="subject" id="subject">
+            </p>
+            <p>
+                <label>Message:</label></br>
+                <textarea name="detail" id="detail" cols=75% rows=5%></textarea>
+            </p>
+            <p>
+                <label>Name:</label></br>
+                <input type="text" name="name" id="name">
+            </p>
+            <p>
+                <label>Email:</label></br>
+                <input name=”customer_mail” type=”text” id=”customer_mail” size=”50″>
+            </p>
+            <p>
+                <input type="submit" name="submit" value="Send">
+                <input type="reset" name="reset" value="Reset">
+            </p>
+        </fieldset>
+    </form>
+</html>
+<!--
+    </table>
+    <table width=”400″ border=”0″ align=”center” cellpadding=”0″ cellspacing=”1″>
+        <tr>
+            <td>
+                <form name=”form1″ method=”post” action=”mailto.php”>
+                    <table width=”100%” border=”0″ cellspacing=”1″ cellpadding=”3″>
+            <tr>
 <td width=”16%”>Subject</td>
 <td width=”2%”>:</td>
 <td width=”82%”><input name=”subject” type=”text” id=”subject” size=”50″></td>
@@ -41,4 +65,4 @@
 </form>
 </td>
 </tr>
-</table>
+</table>-->
